2d52ed7dff615bb8d7629537efaba571f4797a32
Makrovic <44518143+Makrovic@users.noreply.github.com>
2021-12-15 11:03:18 +0700

    home pt

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('load', () => {
     AOS.init({
         duration: 1000,
@@ -14,36 +13,48 @@ const sw_testi = new Swiper('.sw-testi', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
-    pagination: {
-      el: '.swiper-pagination',
-    }
-  });
 
-  const sw_dokumentasi = new Swiper('.sw-dokumentasi', {
+    pagination: {
+        el: '.swiper-pagination',
+    }
+});
+
+const sw_dokumentasi = new Swiper('.sw-dokumentasi', {
     // Optional parameters
     slidesPerView: 3,
     spaceBetween: 30,
-  
-    pagination: {
-      el: '.swiper-pagination',
-    }
-  });
 
-  const sw_bannerpt = new Swiper('.sw-bannerpt', {
+    pagination: {
+        el: '.swiper-pagination',
+    }
+});
+
+const sw_bannerpt = new Swiper('.sw-bannerpt', {
     // Optional parameters
     effect: 'fade',
-  fadeEffect: {
-    crossFade: true
-  },
-  autoplay: {
-    delay: 3000,
-  },
+    fadeEffect: {
+        crossFade: true
+    },
+    autoplay: {
+        delay: 3000,
+    },
     direction: 'horizontal',
     loop: true,
-  
+
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  });
+});
+
+const sw_galeri = new Swiper('.sw-galeri', {
+    autoplay: {
+        delay: 3000,
+    },
+    direction: 'horizontal',
+
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+});
--- resources/views/pt/home.blade.php
+++ resources/views/pt/home.blade.php
@@ -73,7 +73,7 @@
                         <figcaption class="d-flex align-items-center justify-content-center">
                             <h2 class="align-middle">Carica Cemerlang</h2>
                         </figcaption>
-                        <a href="#"></a>
+                        <a href="#home-carica"></a>
                       </figure>
                 </div>
                 <div class="col-12 col-sm-4">
@@ -83,24 +83,24 @@
                         <figcaption class="d-flex align-items-center justify-content-center">
                             <h2 class="align-middle">Bintang Cemerlang Snack</h2>
                         </figcaption>
-                        <a href="#"></a>
+                        <a href="#home-snack"></a>
                       </figure>
                 </div>
                 <div class="col-12 col-sm-4">
                     <figure class="imghvr-zoom-in">
-                        <img src="{{ asset('images/pt/carica/carica.jpg') }}" alt=""
+                        <img src="{{ asset('images/pt/craft/craft.jpg') }}" alt=""
                         class="img-fluid rounded">
                         <figcaption class="d-flex align-items-center justify-content-center">
                             <h2 class="align-middle">Claster Craft Cemerlang</h2>
                         </figcaption>
-                        <a href="#"></a>
+                        <a href="#home-craft"></a>
                       </figure>
                 </div>
             </div>
         </div>
     </section>
 
-    <section class="sec">
+    <section class="sec" id="home-carica">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-sm-12 col-md-6 col-lg-6 my-4" data-aos="fade-left">
@@ -117,7 +117,7 @@
             </div>
         </div>
     </section>
-    <section class="sec back-gray">
+    <section class="sec back-gray" id="home-snack">
         <div class="container">
             <div class="row flex-row-reverse text-end justify-content-center">
                 <div class="col-sm-12 col-md-6 col-lg-6 my-4" data-aos="fade-left">
@@ -134,11 +134,11 @@
             </div>
         </div>
     </section>
-    <section class="sec">
+    <section class="sec" id="home-craft">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-sm-12 col-md-6 col-lg-6 my-4" data-aos="fade-left">
-                    <img src="{{ asset('images/pt/carica/carica.jpg') }}" alt=""
+                <div class="col-sm-12 col-md-6 col-lg-6 my-4" data-aos="fade-right">
+                    <img src="{{ asset('images/pt/craft/craft.jpg') }}" alt=""
                         class="img-fluid rounded my-4">
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6 my-4 d-flex flex-column justify-content-center"
@@ -155,53 +155,17 @@
     <section class="sec back-gray">
         <div class="container">
             <div class="row text-center" data-aos="fade-up">
-                <h1>Testimoni</h1>
+                <h1>Galeri</h1>
             </div>
-            <div class="row mt-5">
-                <div class="swiper sw-testi" data-aos="fade-up">
+            <div class="row mt-5" data-aos="fade-up">
+                <div class="swiper sw-galeri">
                     <div class="swiper-wrapper">
-                        <div class="swiper-slide">
-                            <div class="img-box">
-                                <img src="../assets/testimoni/testi-adin.jpeg" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="text-center">
-                                <p class="fst-italic">Akibat salah pergaulan saya terjebak pada lingkungan yang tidak
-                                    baik, saya DO dari
-                                    sekolah formal, dan melanjutkan di Paket C (setara SMA) . Saya mengucapkan terima
-                                    kasih
-                                    pada PKBM CEMERLANG. Benar benar memiliki rasa tanggung jawab dalam pembinaan
-                                    peserta didiknya. Saya ditempatkan kerja di PT Tirta Investama (AQUA).
-                                    Saya merekomendasikan kepada teman dan saudara, untuk belajar di PKBM CEMERLANG.
-                                    Selain mendapatkan ijazah, saya dan rekan-rekan tidak menganggur lagi.</p>
-                                <p class="fw-bold">Adin - Lulusan Paket C PKBM CEMERLANG</p>
-                            </div>
+                        @for ($i = 1; $i <= 6; $i++)
+                        <div class="swiper-slide d-flex justify-content-center align-items-center">
+                            <a href="{{asset('images/pt/galeri/'.$i.'.jpg')}}" data-lightbox="image-1">
+                            <img class="flex-auto img-fluid rounded" src="{{ asset('images/pt/galeri/'.$i.'.jpg') }}"></a>
                         </div>
-                        <div class="swiper-slide">
-                            <div class="img-box">
-                                <img src="../assets/testimoni/testi-mardiati.jpeg" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="text-center">
-                                <p class="fst-italic">Tidak menyangka mengikuti pembelajaran di PKBM CEMERLANG begitu
-                                    menyenangkan.
-                                    Tutor sangat peduli pada kami, adanya layanan konseling , dharma wisata,
-                                    dan di luar jam akademik kesetaraan kami dibimbing keterampilan untuk bekal kerja
-                                    atau berwirausaha.PKBM CEMERLANG benar-benar mengerti apa yang kami butuhkan.</p>
-                                <p class="fw-bold">Mardiati - Lulusan Paket B PKBM CEMERLANG</p>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="img-box">
-                                <img src="../assets/testimoni/testi-afifah.jpeg" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="text-center">
-                                <p class="fst-italic">Setelah mengikuti keterampilan di PKBM CEMERLANG saya dibina untuk
-                                    berwirausaha.
-                                    Alhamdulilah usaha sukses, saya membuka 2 toko pusat oleh-oleh
-                                    di Jl. Raya Kertek - Wonosobo.
-                                    Terimakasih PKBM CEMERLANG</p>
-                                <p class="fw-bold">Afifah - Lulusan Keterampilan PKBM CEMERLANG</p>
-                            </div>
-                        </div>
+                        @endfor
                     </div>
                     <div class="swiper-pagination"></div>
                 </div>
@@ -209,44 +173,7 @@
         </div>
     </section>
     {{-- endsection --}}
-
-    {{-- script pie chart --}}
-    <script>
-        var data = [{
-            data: [80, 90, 90, 85],
-            backgroundColor: [
-                "#3f51b5",
-                "#9c9c00",
-                "#17246e",
-                "#e0e000"
-            ],
-            borderColor: "#fff"
-        }];
-
-        var options = {
-            responsive: true,
-            plugins: {
-                datalabels: {
-                    formatter: (value, ctx) => {
-                        let percentage = value + "%";
-                        return percentage;
-                    },
-                    color: '#fff',
-                }
-            }
-        };
-
-        var ctx = document.getElementById("myChart").getContext('2d');
-        var myChart = new Chart(ctx, {
-            plugins: [ChartDataLabels],
-            type: 'pie',
-            data: {
-                labels: ["Praktikum", "Education", "Learning", "Motivation"],
-                datasets: data
-            },
-            options: options
-        });
-
-    </script>
-    {{-- endscript --}}
+    @stop
+    @section('customjs')
+    <script src="{{ asset('js/lightbox.js') }}"></script>
     @stop
