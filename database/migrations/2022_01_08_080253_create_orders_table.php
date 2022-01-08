08c8136738ffa94be627291f6e2fed836afd1fa9
Makrovic <mail.faisalmakrufgmail.com>
2022-01-08 15:30:58 +0700

    cart & order model & migration

---
+++ database/migrations/2022_01_08_080253_create_orders_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_transaksi')->unique();
+            $table->dateTime('tgl_transaksi');
+            $table->string('buyer');
+            $table->string('nohp');
+            $table->text('alamat');
+            $table->string('kota');
+            $table->integer('total_produk');
+            $table->integer('total_produk');
+            $table->integer('berat');
+            $table->integer('subtotal');
+            $table->integer('ongkir');
+            $table->integer('total');
+            $table->text('catatan')->nullable();
+            $table->enum('status', ['0', '1', '2'])->default('0');
+            $table->dateTime('tgl_pembayaran')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
